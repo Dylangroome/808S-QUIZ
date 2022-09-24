60073e1a1a940c08b195cbb94e555b14b505f28c
Dylangroome <dylangroome13@gmail.com>
2022-09-24 22:37:03 +0000

    add for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -52,5 +52,45 @@ startGame = () => {
     questionCounter = 0
     score = 0
     availableQuestions = [...questions]
-    getNewQuestions()
+    getNewQuestion()
 }
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('MostRecentScore', score)
+
+        return window.localStorage.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click' , e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers - false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ?  'correct' : 'incorrect'
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+    })
+})

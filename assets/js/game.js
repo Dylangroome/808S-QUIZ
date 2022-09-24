24e3e782c645a57d7c3b6115164a9c398791a86a
Dylangroome <dylangroome13@gmail.com>
2022-09-24 21:59:42 +0000

    add java script question

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []

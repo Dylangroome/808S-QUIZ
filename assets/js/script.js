7769ed826253e6c56792efc7293f4cbe5db63f94
Dylangroome <dylangroome13@gmail.com>
2022-10-03 21:43:57 +0000

    add audio

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,29 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+/**
+ * Music file setup
+ */
+ let bgmusic = document.getElementById('bgMusic');
+ bgmusic.volume = 0.5;
+ bgmusic.loop = true;
+ let soundOn = false;
+ 
+ /**
+  * Toggle on and off background music
+  */
+ function musicControl() {
+     soundOn = !soundOn;
+     if (soundOn) {
+         bgmusic.play();
+         document.getElementById("music-on").classList.add('hide');
+         document.getElementById("music-off").classList.remove('hide');
+     } else {
+         bgmusic.pause();
+         document.getElementById("music-off").classList.add('hide');
+         document.getElementById("music-on").classList.remove('hide');
+     }
+ }
+ 

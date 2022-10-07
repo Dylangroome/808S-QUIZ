5f8f1f9c1c1783a6000574b43c15d13e691eb07d
Dylangroome <dylangroome13@gmail.com>
2022-10-07 05:17:17 +0000

    add end page

--- assets/js/game.js
+++ assets/js/game.js
@@ -59,7 +59,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.localStorage.assign('/end.html')
+        return window.location.assign('/end.html')
     }
 
     questionCounter++
